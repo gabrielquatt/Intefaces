8c5916477a9e21ebb3b8ac644a01c36e8110378f
Facundo Arana <facundoaranaloberia@gmail.com>
2021-09-10 20:28:57 -0300

    boton reset funcionando y codigo mejor comentado

--- TP1/EntregaFinal/js/main.js
+++ TP1/EntregaFinal/js/main.js
@@ -1,177 +1,181 @@
 /** @type { HTMLcanvasElement} */
 let canvas = document.getElementById("canvas");
 
+/** Contexto */
 let ctx = canvas.getContext("2d");
-const width = canvas.width;
-const height = canvas.height;
 
-document.getElementById("btnSave").addEventListener("click", saveImage);
+/** Tamaño maximo de canvas */
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+/* Imagen */
+let IMG = new Image();
+
+/** Proporcion de la imagen */
+let PROP = 1;
+
+/** Tamaño temporal del canvas ajustado a la imagen */
+let TEMP_WIDTH = WIDTH;
+let TEMP_HEIGHT = HEIGHT;
+
+/** Input (oculto) para seleccionar imagen del disco */
 let inputFile = document.getElementById("file");
 
+/** Boton visible */
 document.getElementById("btn").addEventListener("click", () => {
   inputFile.click();
 });
 
+/** Evento que dispara carga de la imagen del disco */
 inputFile.addEventListener("change", (e) => loadImage(e));
 
+/** Evento que dispara la descarga de la imagen */
+document.getElementById("btnSave").addEventListener("click", saveImage);
+
+/** Restaurar imagen original */
+document.getElementById("reset").addEventListener("click", drawImage);
+
+/**
+ * Usuario selecciona una imagen de su disco
+ * @param { Event } e
+ */
 function loadImage(e) {
   if (e.target.files) {
+    // Archivo seleccionado
     let file = e.target.files[0];
 
-    // puede ser global
-    let img = new Image();
-
     let fileReader = new FileReader();
-    fileReader.onload = (e) => (img.src = e.target.result);
+
+    // Define 'src' de imagen con la ruta del archivo seleccionado
+    fileReader.onload = (e) => (IMG.src = e.target.result);
     fileReader.readAsDataURL(file);
-    img.onload = () => drawImage(img);
+
+    // Dibujar imagen
+    IMG.onload = () => drawImage();
   }
 }
 
-function drawImage(img) {
-  clearCanvas();
-  let prop = 1;
-  if (img.width > width || img.height > height) {
-    prop = resizeImage(img);
-  }
-  let newWidth = img.width * prop;
-  let newHeight = img.height * prop;
-  canvas.width = newWidth;
-  canvas.height = newHeight;
+/**
+ * Muestra la imagen en el canvas
+ */
+function drawImage() {
+  resetCanvas();
+  setSize();
   ctx.imageSmoothingEnabled = true;
-  ctx.drawImage(img, 0, 0, newWidth, newHeight);
+  ctx.drawImage(IMG, 0, 0, TEMP_WIDTH, TEMP_HEIGHT);
 }
 
-function resizeImage(img) {
-  let percentW = 1;
-  let percentH = 1;
-  if (img.width > width) {
-    percentW = (width - img.width) / img.width;
-  }
-  if (img.height > height) {
-    percentH = (height - img.height) / img.height;
-  }
-  let r = percentW < percentH ? percentW : percentH;
-  return 1 - r * -1;
+/**
+ * Definicion de proporciones de la imagen dependiendo su tamaño
+ */
+function setSize() {
+  PROP = IMG.width > WIDTH || IMG.height > HEIGHT ? aspectRatio() : 1;
+
+  TEMP_WIDTH = IMG.width * PROP;
+  TEMP_HEIGHT = IMG.height * PROP;
+
+  // Nuevas dimensiones del canvas (para mejor descarga de imagen)
+  canvas.width = TEMP_WIDTH;
+  canvas.height = TEMP_HEIGHT;
 }
 
-function clearCanvas() {
-  canvas.width = width;
-  canvas.height = height;
-  ctx.clearRect(0, 0, width, height);
+/**
+ * Si la imagen es mayor al tamaño maximo del canvas
+ * Esta funcion determina que porcentaje de la imagen entra en el canvas
+ * @param { Image } img
+ */
+function aspectRatio() {
+  let w = IMG.width > WIDTH ? (WIDTH - IMG.width) / IMG.width : 1;
+  let h = IMG.height > HEIGHT ? (HEIGHT - IMG.height) / IMG.height : 1;
+  return 1 - (w < h ? w : h) * -1;
 }
 
+/**
+ * Borra el contenido y restaura dimensiones originales del canvas
+ */
+function resetCanvas() {
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
 
-//----------------------------------------------------------------------------------------------//
-// imageData(): se usa para crear el objeto.
-// getImageData(): método de imageData para copiar los pixels de una región del canvas
-// putImageData(): méodo de imageData para dibujar en el canvas una imagen a partir de sus pixels
-//----------------------------------------------------------------------------------------------//
+function getCopy() {
+  ctx.drawImage(IMG, 0, 0, TEMP_WIDTH, TEMP_HEIGHT);
+  return ctx.getImageData(0, 0, TEMP_WIDTH, TEMP_HEIGHT);
+}
 
 /**
  * Sepia:
  * La fotografía en sepia no es más que una imagen en blanco y negro con un matiz marrón que
  * crea una sensación de calidez.
  */
-document.getElementById('btn_sepia').addEventListener("click", () => {
-  let imageEditada = sepia();
-  ctx.putImageData(imageEditada, 0, 0);
-});
+document.getElementById("btn_sepia").addEventListener("click", sepia);
 
-//----------------------------------------------------------------------------------------------//
+function sepia() {
+  let c = getCopy();
+  for (let x = 0; x < TEMP_HEIGHT; x++) {
+    for (let y = 0; y < TEMP_WIDTH; y++) {
+      let arr = getPixel(c, x, y);
+      let promPxR = 0.393 * arr[0] + 0.769 * arr[1] + 0.189 * arr[2];
+      let promPxG = 0.349 * arr[0] + 0.686 * arr[1] + 0.168 * arr[2];
+      let promPxB = 0.272 * arr[0] + 0.534 * arr[1] + 0.131 * arr[2];
+      let promPxA = 255;
+      setPixel(c, x, y, promPxR, promPxG, promPxB, promPxA);
+    }
+  }
+  ctx.putImageData(c, 0, 0);
+}
+
 /**
  * Binarizacion:
  * Una imágen que solo sea representada por dos tonos de color, por general: blanco y negro.
  */
- document.getElementById('btn_binarizacion').addEventListener("click", () => {
-  let imageEditada = binarizacion();
-  ctx.putImageData(imageEditada, 0, 0);
-});
+document.getElementById("btn_binary").addEventListener("click", binarizacion);
 
-//----------------------------------------------------------------------------------------------//
-/**
- * Negativo
- * Un negativo es una imagen fotográfica en la que las luces aparecen en tonos oscuros y 
- * las sombras en tonos claros
- */
- document.getElementById('btn_negativo').addEventListener("click", () => {
-  let imageEditada = negativo();
-  ctx.putImageData(imageEditada, 0, 0);
-});
-//----------------------------------------------------------------------------------------------//
-/**
- * Setea copia de Canvas con colores Blanco o Negro segun el promedio de cada pixel
- * @returns 
- */
- function binarizacion() {
-  // usamos getImageData para copiar el canvas
-  let copiaCanvas = ctx.getImageData(0, 0, width, height);
-  for (let x = 0; x < height; x++) {
-      for (let y = 0; y < width; y++) {
-          let arrRGBA = getPixel(copiaCanvas, x, y);
-          let result = white_or_dark(Math.floor((arrRGBA[0] + arrRGBA[1] + arrRGBA[2]) / 3));
-          let pixelR = result;
-          let pixelG = result;
-          let pixelB = result;
-          let pixelA = 255;
-          setPixel(copiaCanvas, x, y, pixelR, pixelG, pixelB, pixelA);
-      }
-  }
-  return copiaCanvas;
-}
-
-//----------------------------------------------------------------------------------------------//
-/**
- * Setea copia de Canvas con tonalidades Sepias
- * @returns 
- */
-function sepia() {
-  // usamos getImageData para copiar el canvas
-  let copiaCanvas = ctx.getImageData(0, 0, width, height);
-  for (let x = 0; x < height; x++) {
-    for (let y = 0; y < width; y++) {
-      let arrRGBA = getPixel(copiaCanvas, x, y);
-      //formula para comvertir pixeles a "Sepia"
-      //(0.393 * R )+( 0.769 * G) + (0.189 * B)
-      //(0.349 * R) + (0.686 * G) + (0.168 * B)
-      //(0.272 * R) + (0.534 * G) + (0.131 * B)
-      let promPixelR = (0.393 * arrRGBA[0]) + (0.769 * arrRGBA[1]) + (0.189 * arrRGBA[2]);
-      let promPixelG = (0.349 * arrRGBA[0]) + (0.686 * arrRGBA[1]) + (0.168 * arrRGBA[2]);
-      let promPixelB = (0.272 * arrRGBA[0]) + (0.534 * arrRGBA[1]) + (0.131 * arrRGBA[2]);
-      let promPixelA = 255;
-      setPixel(copiaCanvas, x, y, promPixelR, promPixelG, promPixelB, promPixelA);
+function binarizacion() {
+  let c = getCopy();
+  for (let x = 0; x < TEMP_HEIGHT; x++) {
+    for (let y = 0; y < TEMP_WIDTH; y++) {
+      let arrRGBA = getPixel(c, x, y);
+      let result = prom(arrRGBA) > 127 ? 255 : 0;
+      let pixelR = result;
+      let pixelG = result;
+      let pixelB = result;
+      let pixelA = 255;
+      setPixel(c, x, y, pixelR, pixelG, pixelB, pixelA);
     }
   }
-  return copiaCanvas;
+  ctx.putImageData(c, 0, 0);
 }
 
-//----------------------------------------------------------------------------------------------//
 /**
- * Setea copia de Canvas a modo negativo
- * @returns 
+ * Negativo
+ * Un negativo es una imagen fotográfica en la que las luces aparecen en tonos oscuros y
+ * las sombras en tonos claros
  */
+document.getElementById("btn_negativo").addEventListener("click", negativo);
+
 function negativo() {
-   // usamos getImageData para copiar el canvas
-  let copiaCanvas = ctx.getImageData(0, 0, width, height); 
-  for (let x = 0; x < height; x++) {
-      for (let y = 0; y < width; y++) {
-          let arrRGBA = getPixel(copiaCanvas, x, y);
-          let promPixelR = 255 - arrRGBA[0];
-          let promPixelG = 255 - arrRGBA[1];
-          let promPixelB = 255 - arrRGBA[2];
-          let promPixelA = 255;
-          setPixel(copiaCanvas, x, y, promPixelR, promPixelG, promPixelB, promPixelA);
-      }
+  let c = getCopy();
+  for (let x = 0; x < TEMP_HEIGHT; x++) {
+    for (let y = 0; y < TEMP_WIDTH; y++) {
+      let arrRGBA = getPixel(c, x, y);
+      let promPixelR = 255 - arrRGBA[0];
+      let promPixelG = 255 - arrRGBA[1];
+      let promPixelB = 255 - arrRGBA[2];
+      let promPixelA = 255;
+      setPixel(c, x, y, promPixelR, promPixelG, promPixelB, promPixelA);
+    }
   }
-  return copiaCanvas;
+  ctx.putImageData(c, 0, 0);
 }
-//----------------------------------------------------------------------------------------------//
+
 /**
  * Devuelve arreglo de pixel con los colores Red, Grenn, Blue y Alpha
  */
 function getPixel(imageData, x, y) {
   let index = (x + y * imageData.height) * 4;
-  let r = imageData.data[index + 0];
+  let r = imageData.data[index];
   let g = imageData.data[index + 1];
   let b = imageData.data[index + 2];
   let a = imageData.data[index + 3];
@@ -183,27 +187,24 @@ function getPixel(imageData, x, y) {
  */
 function setPixel(imageData, x, y, r, g, b, a) {
   let index = (x + y * imageData.height) * 4;
-  imageData.data[index + 0] = r;
+  imageData.data[index] = r;
   imageData.data[index + 1] = g;
   imageData.data[index + 2] = b;
   imageData.data[index + 3] = a;
 }
 
 /**
- * Devuelve el valor 0 o 255 segun cuan serca este el promedio del pixel de ese valor
- * @param {*} pixel //promedio del pixel
- * @returns 
+ * @returns promedio de colores de un pixel dado en un arreglo
+ * @param { Array } arr
  */
-function white_or_dark(pixel) {
-  if ((pixel > 127) && (pixel <= 255)) {
-      return 255;
-  } else if ((pixel >= 0) && (pixel <= 127)) {
-      return 0;
-  }
+function prom(arr) {
+  return Math.floor((arr[0] + arr[1] + arr[2]) / 3);
 }
 
+/**
+ * Descargar imagen
+ */
 function saveImage() {
-  // save image as png
   let link = document.createElement("a");
   link.download = "canvas.png";
   link.href = canvas
@@ -211,4 +212,3 @@ function saveImage() {
     .replace("image/png", "image/octet-stream");
   link.click();
 }
-
